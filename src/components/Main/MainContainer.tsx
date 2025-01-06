babf2efa462391f5a7521924b7474c885650b883
aid003 <teremochek859@gmail.com>
2025-01-06 16:20:13 +0300

    dev

--- src/components/Main/MainContainer.tsx
+++ src/components/Main/MainContainer.tsx
@@ -9,7 +9,7 @@ export default async function MainContainer() {
   return (
     <div className={styles.mainContainer}>
       <div className={styles.leftContainer}>
-        <h1 className={styles.heading}>Текущие проекты</h1>
+        <h1 className={styles.heading}>Проекты</h1>
         <Suspense fallback={<ProjectsListSkeleton />}>
           <ProjectsList />
         </Suspense>
